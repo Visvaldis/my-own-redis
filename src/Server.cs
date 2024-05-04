4540f5ef7946209980273626159d45adc5fe313e
Vsevolod <sevoslav@gmail.com>
2024-05-04 14:57:02 -0400

    Add commands array

--- src/Server.cs
+++ src/Server.cs
@@ -15,8 +15,11 @@ try
         string data = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
         data = data.Trim();
         Console.WriteLine(data);
-
+        var commands = data.Split('\n');
+        foreach (var command in commands)
+        {
             client.Send("+PONG\r\n"u8.ToArray()); // send response
+        }
     }
 }
 catch(Exception ex)
