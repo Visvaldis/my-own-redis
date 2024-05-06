c9a07611243b29e563dfdf96d0723abd2d57ff65
Vsevolod <sevoslav@gmail.com>
2024-05-05 22:24:32 -0400

    wip: Adding eventloop

---
+++ src/Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src
+{
+	internal class Event
+	{
+		public int Id { get; set; }
+
+		public string? Name { get; set; }
+
+		public Socket? Client { get; set; }
+
+		public string? Body { get; set; }
+
+		//should be abstract
+		public void ProcessEvent()
+		{
+			//do stuff
+		}
+	}
+}
---
+++ src/EventLoop.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src
+{
+	internal class EventLoop
+	{
+		Queue<Event> queue;
+		Thread thread;
+		
+		public void AddEvent(Event @event)
+		{
+			queue.Enqueue(@event);
+		}
+
+		public void Start()
+		{
+			//var thread = new Thread(Process)
+		}
+
+		public void Stop()
+		{
+			//thread stop
+			// queue write down or clear
+		}
+
+		private void Process()
+		{
+
+			if (queue.Count == 0)
+			{
+				// sleep
+			}
+
+			var current = queue.Dequeue();
+			current.ProcessEvent();
+		}
+
+
+	}
+}
